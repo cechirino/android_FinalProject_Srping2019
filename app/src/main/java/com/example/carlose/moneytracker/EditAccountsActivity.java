74e9ce447528213b0d7797aa06754c2524d00e36
cechirino <cechirino96@gmail.com>
2019-03-05 15:24:52 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/carlose/moneytracker/EditAccountsActivity.java
@@ -0,0 +1,12 @@
+package com.example.carlose.moneytracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditAccountsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/example/carlose/moneytracker/MainActivity.java
+++ app/src/main/java/com/example/carlose/moneytracker/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.carlose.moneytracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menu = (Button) findViewById(R.id.menuButton);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void CallActivity(){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
