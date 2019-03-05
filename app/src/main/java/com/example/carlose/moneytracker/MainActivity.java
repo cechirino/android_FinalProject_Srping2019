72290ab0df4c817361cbde676da64bb45a09f4fb
cechirino <cechirino96@gmail.com>
2019-03-05 16:12:57 -0600

    Initial Commit

--- app/src/main/java/com/example/carlose/moneytracker/MainActivity.java
+++ app/src/main/java/com/example/carlose/moneytracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.carlose.moneytracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,6 +11,9 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button menu;
+    private CardView expensesCardView;
+    private CardView budgetCardview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +24,24 @@ public class MainActivity extends AppCompatActivity {
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                CallActivity();
             }
         });
+
+        expensesCardView = (CardView) findViewById(R.id.expensesCardView);
+
+        expensesCardView.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                expensesCardView.getContext().startActivity(new Intent(expensesCardView.getContext(), EditExpensesActivity.class));
+            }
+        });
+    }
+
+    private void CallExpensesEditLayout()
+    {
+        Intent intent = new Intent(getApplicationContext(), EditExpensesActivity.class);
+        startActivity(intent);
     }
 
     private void CallActivity(){
